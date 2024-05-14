6805df01128fb831a5ca083d5a1744f0ab81ac27
Yue Huang <806628409@qq.com>
2024-05-14 15:36:07 +0800

    Update setGeneralYLabel.m

--- EasyPlot/+EasyPlot/setGeneralYLabel.m
+++ EasyPlot/+EasyPlot/setGeneralYLabel.m
@@ -3,6 +3,16 @@ function handle = setGeneralYLabel(axes_all, label, varargin)
         axes_all = {axes_all};
     end
     width = 1.5;
+    height = 5;
+    if nargin>2
+        for k = 1:2:size(varargin,2)
+            if strcmpi(varargin{k},'Height')
+                height = varargin{k+1};
+            elseif strcmpi(varargin{k},'Width')
+                width = varargin{k+1};
+            end
+        end
+    end
 
     posTop = -1e8;
     posBottom = 1e8;
@@ -13,9 +23,10 @@ function handle = setGeneralYLabel(axes_all, label, varargin)
         posBottom = min(posBottom, axes_all{k}.Position(2));
     end
     posLeft = axes_all{1}.Position(1)-width;
+    pos_vertical_center = 0.5*(posTop+posBottom);
 
     % compute the position after rotating 90 degree at left-bottom corner
-    pos = [posLeft+width, posBottom, posTop-posBottom, width];
+    pos = [posLeft+width, pos_vertical_center-height/2, height, width];
     
     handle = EasyPlot.annotation(fig, 'textbox',...
         'FitBoxToText','on',...
@@ -41,12 +52,14 @@ function handle = setGeneralYLabel(axes_all, label, varargin)
                 handle.UserData.MarginTop = varargin{k+1};
             elseif strcmpi(varargin{k},'MarginBottom')
                 handle.UserData.MarginBottom = varargin{k+1};
-            elseif strcmpi(varargin{k},'Height')
-                handle.Position(4) = varargin{k+1};
             elseif strcmpi(varargin{k},'xShift')
                 handle.Position(1) = handle.Position(1)+varargin{k+1}; % because of rotation
             elseif strcmpi(varargin{k},'yShift')
                 handle.Position(2) = handle.Position(2)+varargin{k+1}; % because of rotation
+            elseif strcmpi(varargin{k},'Height')
+
+            elseif strcmpi(varargin{k},'Width')
+
             else
                 EasyPlot.set(handle,varargin{k},varargin{k+1});
             end
